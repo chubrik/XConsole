84e6998a59bd0e454277824ffc5f86bcd6c5116f
Arkadi Chubrik <arkadi@chubrik.org>
2022-11-05 01:02:00 +0400

    Build properties & demo

--- XConsole.Demo/Program.cs
+++ XConsole.Demo/Program.cs
@@ -1,9 +1,7 @@
-﻿#if NET5_0
-#pragma warning disable CA1416 // Validate platform compatibility
-#endif
-
+﻿using Chubrik.XConsole.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -40,35 +38,37 @@ public static class Program
         Console.WriteLine();
         //
 
+        Console.Utils.MaximizeWindow();
         //Console.SetCursorPosition(0, 8950);
 
-#if !NETSTANDARD1_3
         var sysFolder = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.System));
-#else
-        var sysFolder = new DirectoryInfo(@"C:\Windows\System32");
-#endif
         var files = sysFolder.GetFiles("*.exe");
         var fileIndex = 0;
         Console.Pin(() => new[] { "m`This is pin!\n", $"g`Number of files: ", $"W`{fileIndex}" });
         var endPosList = new List<ConsolePosition>();
         var animationList = new List<ConsoleAnimation>();
+        var random = new Random();
 
         for (; fileIndex < Math.Min(100, files.Length); fileIndex++)
         {
             await Task.Delay(50);
-            var endPos = Console.WriteLine(files[fileIndex].Name + ' ').End;
+            var color = Color.FromArgb(random.Next(255), random.Next(255), random.Next(255));
+            var bgColor = Color.FromArgb(random.Next(96), random.Next(96), random.Next(96));
+            var endPos = Console.WriteLine(files[fileIndex].Name.Color(color).BgColor(bgColor) + ' ').End;
             endPosList.Add(endPos);
 
             if ((fileIndex + 1) % 10 == 0)
             {
-                var animation = endPosList[fileIndex - 7].AnimateEllipsis();
-                animationList.Add(animation);
+                var animation1 = endPosList[fileIndex - 7].AnimateEllipsis();
+                var animation2 = endPosList[fileIndex - 2].AnimateSpinner();
+                animationList.Add(animation1);
+                animationList.Add(animation2);
             }
         }
 
         foreach (var animation in animationList)
         {
-            await Task.Delay(300);
+            await Task.Delay(150);
             animation.Stop().TryWrite("Cb`ok");
         }
     }
