9fe64bab0fbbd0fb3da5f2c437043abe78cc9f4c
Arkadi Chubrik <arkadi@chubrik.org>
2025-10-09 19:28:25 +0400

    Small fixes

--- XConsole/XConsole.cs
+++ XConsole/XConsole.cs
@@ -1,6 +1,8 @@
 ﻿#pragma warning disable CS1573 // Parameter has no matching param tag in the XML comment (but other parameters do)
 #if NET
+#pragma warning disable IDE0079 // Remove unnecessary suppression
 #pragma warning disable CA1416 // Validate platform compatibility
+#pragma warning restore IDE0079 // Remove unnecessary suppression
 #else
 #pragma warning disable CS8604 // Possible null reference argument.
 #endif
@@ -11,14 +13,14 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-#if NET
-using System.Runtime.Versioning;
-#endif
 #if NET9_0_OR_GREATER
 using System.Threading;
 #endif
 #if NET7_0_OR_GREATER
 using System.Diagnostics.CodeAnalysis;
+#endif
+#if NET
+using System.Runtime.Versioning;
 #endif
 
 /// <summary>
@@ -61,7 +63,7 @@ public static class XConsole
     private static readonly object _syncLock = new();
 #endif
 
-    private static bool _coloringEnabled = Environment.GetEnvironmentVariable("NO_COLOR") == null;
+    private static bool _coloringEnabled = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("NO_COLOR"));
     private static bool _cursorVisible;
     private static int _maxTop;
     private static long _shiftTop = 0;
