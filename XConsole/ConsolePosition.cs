3a2391f6478aa845ddd2ec9ecf4d7c8560b3c505
Arkadi Chubrik <arkadi@chubrik.org>
2025-11-11 21:24:06 +0300

    Remove ConsolePosition extra extensions

--- XConsole/ConsolePosition.cs
+++ XConsole/ConsolePosition.cs
@@ -1,6 +1,7 @@
 ﻿namespace Chubrik.XConsole;
 
 using System;
+using System.Collections.Generic;
 #if NET
 using System.Runtime.Versioning;
 #endif
@@ -17,10 +18,10 @@ using System.Runtime.Versioning;
 /// </para>
 /// </summary>
 /// <remarks>
-/// See also these extension methods that write text directly to a position
+/// See also these methods that write text directly to a position
 /// and return the new <see cref="ConsolePosition"/> structure:
-/// <br/>• <seealso cref="ConsolePositionExtensions.Write(ConsolePosition, string?)"/>
-/// <br/>• <seealso cref="ConsolePositionExtensions.TryWrite(ConsolePosition, string?)"/>
+/// <br/>• <seealso cref="Write(string?)"/>
+/// <br/>• <seealso cref="TryWrite(string?)"/>
 /// </remarks>
 #if NET
 [UnsupportedOSPlatform("android")]
@@ -30,6 +31,8 @@ using System.Runtime.Versioning;
 #endif
 public readonly struct ConsolePosition
 {
+    #region Common
+
     /// <summary>
     /// The column position within the console buffer area.
     /// </summary>
@@ -85,4 +88,83 @@ public readonly struct ConsolePosition
 #pragma warning restore CS1734 // XML comment has a paramref tag, but there is no parameter by that name
     [Obsolete("Arguments should be specified.", error: true)]
     public ConsolePosition() => throw new InvalidOperationException();
+
+    #endregion
+
+    #region White methods
+
+    /// <returns>The new end <see cref="ConsolePosition"/> structure.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    /// <inheritdoc cref="TryWrite(string?)"/>
+    public ConsolePosition Write(string? value)
+    {
+        return XConsole.WriteToPosition(this, [ConsoleItem.Parse(value)]);
+    }
+
+    /// <returns>The new end <see cref="ConsolePosition"/> structure.</returns>
+    /// <exception cref="ArgumentOutOfRangeException"/>
+    /// <inheritdoc cref="TryWrite(IReadOnlyList{string?})"/>
+    public ConsolePosition Write(IReadOnlyList<string?> values)
+    {
+        var items = new ConsoleItem[values.Count];
+
+        for (var i = 0; i < values.Count; i++)
+            items[i] = ConsoleItem.Parse(values[i]);
+
+        return XConsole.WriteToPosition(this, items);
+    }
+
+    /// <summary>
+    /// Wtites the specified string directly to a position.
+    /// Text can be colored using a simple <see href="https://github.com/chubrik/XConsole#coloring">microsyntax</see>.
+    /// </summary>
+    /// <param name="value">
+    /// The value to write.
+    /// Text can be colored using a simple <see href="https://github.com/chubrik/XConsole#coloring">microsyntax</see>.
+    /// </param>
+    /// <returns>
+    /// The new end <see cref="ConsolePosition"/> structure,
+    /// or <see langword="null"/> if the current position actually points outside the console buffer area.
+    /// </returns>
+    /// <exception cref="System.Security.SecurityException"/>
+    /// <exception cref="System.IO.IOException"/>
+    public ConsolePosition? TryWrite(string? value)
+    {
+        try
+        {
+            return XConsole.WriteToPosition(this, [ConsoleItem.Parse(value)]);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Wtites the specified set of string values directly to a position.
+    /// Text can be colored using a simple <see href="https://github.com/chubrik/XConsole#coloring">microsyntax</see>.
+    /// </summary>
+    /// <param name="values">
+    /// The set of values to write.
+    /// Text can be colored using a simple <see href="https://github.com/chubrik/XConsole#coloring">microsyntax</see>.
+    /// </param>
+    /// <inheritdoc cref="TryWrite(string?)"/>
+    public ConsolePosition? TryWrite(IReadOnlyList<string?> values)
+    {
+        var items = new ConsoleItem[values.Count];
+
+        for (var i = 0; i < values.Count; i++)
+            items[i] = ConsoleItem.Parse(values[i]);
+
+        try
+        {
+            return XConsole.WriteToPosition(this, items);
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            return null;
+        }
+    }
+
+    #endregion
 }
